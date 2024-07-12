954eabe67291515402b3a8d1d3ba94aea6d1babf
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-11 23:16:36 -0500

    feat: Add new authentication feature for user login.

--- commit.sh
+++ commit.sh
@@ -4,6 +4,8 @@ git add -A
 
 message=$(git --no-pager diff | jq -Rs '{"diff": .}' | curl -s -X POST "http://localhost" -H "Content-Type: application/json" -d @- | jq -r '.message')
 
+echo "$message"
+
 if [ -z "$message" ]; then
     echo "Aborting commit due to empty commit message."
     exit 1
