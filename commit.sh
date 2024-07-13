160d6872f1ebcbdaa68f95468fb7d756a50f3767
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-12 21:02:30 -0500

    style: Remove unnecessary escape characters and simplify echo statements

--- commit.sh
+++ commit.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
-# Color variables
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 YELLOW="\033[0;33m"
 NC="\033[0m" # No Color
 
-# Function to get the commit message from the server
 get_commit_message() {
     response=$(git --no-pager diff --cached | jq -Rs '{"diff": .}' | curl -s -w "\n%{http_code}" -X POST "http://localhost" -H "Content-Type: application/json" -d @-)
     http_status=$(echo "$response" | tail -n1)
     message=$(echo "$response" | sed '$d' | jq -r '.message')
 }
 
-# Function to commit with the provided message
 commit_with_message() {
     local commit_message=$1
     if [ -z "$commit_message" ]; then
-        echo -e "${RED}Aborting commit due to empty commit message.${NC}"
+        echo "${RED}Aborting commit due to empty commit message.${NC}"
         exit 1
     else
         git commit -m "$commit_message" --no-verify
-        echo -e "${GREEN}Commit successful with message: $commit_message${NC}"
+        echo "${GREEN}$commit_message${NC}"
         exit 0
     fi
 }
 
-# Function to prompt for a custom commit message
 prompt_for_custom_message() {
     read -p "Enter custom commit message: " custom_message < /dev/tty
     commit_with_message "$custom_message"
 }
 
-# Main loop to get commit message and confirm with the user
 while true; do
     get_commit_message
 
@@ -40,7 +35,7 @@ while true; do
         echo -e "${RED}Failed to get commit message from server or empty message.${NC}"
         prompt_for_custom_message
     else
-        echo -e "${YELLOW}Received commit message: $message${NC}"
+        echo "${YELLOW}$message${NC}"
         read -p "Do you want to use this commit message? (y/n/r, Enter for yes): " confirm < /dev/tty
 
         case "$confirm" in
@@ -54,7 +49,7 @@ while true; do
                 continue
                 ;;
             * )
-                echo -e "${RED}Invalid option. Please enter y, n, or r.${NC}"
+                echo "${RED}Invalid option. Please enter y, n, or r.${NC}"
                 ;;
         esac
     fi
