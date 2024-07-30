dba23fb99e0f9d2bf8269c10e9a766ece4c314a1
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-29 23:13:23 -0500

    refactor: Simplify HTML generation in handler and middleware functions.

--- src/handler.ts
+++ src/handler.ts
@@ -24,22 +24,12 @@ export function getIndexHandler(
 				return res.status(200).json({ message: `${message} ${command}` });
 			}
 
-			const html = `
-				<!DOCTYPE html>
-				<html lang="en">
-				<head>
-						<meta charset="UTF-8">
-						<meta http-equiv="X-UA-Compatible" content="IE=edge">
-						<meta name="viewport" content="width=device-width, initial-scale=1.0">
-						<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🤖</text></svg>"></link>
-						<script defer data-domain="commit.jaw.dev" src="https://plausible.jaw.dev/js/script.js"></script>
-						<title>commit</title>
-				</head>
-				<body>
-					<p>${message} <span style="background-color: #ededed; border-radius: 5px; padding: 5px 10px 5px 10px">${command}</span></p>
-				</body>
-				</html>`;
-			return res.setHeader('Content-Type', 'text/html').status(200).send(html);
+			return res
+				.setHeader('Content-Type', 'text/html')
+				.status(200)
+				.send(
+					`<p>${message} <span style="background-color: #ededed; border-radius: 5px; padding: 5px 10px 5px 10px">${command}</span></p>`,
+				);
 		}
 
 		let file = cache.get(commitDotShPath);
--- src/middleware.ts
+++ src/middleware.ts
@@ -46,18 +46,41 @@ export function errorMiddleware(error: Error, req: Request, res: Response, next:
 
 	for (const [ErrorClass, statusCode] of errorMap) {
 		if (error instanceof ErrorClass) {
-			return res.status(statusCode).json({ message: error.message });
+			if (req.get('Content-Type') === 'application/json') {
+				return res.status(statusCode).json({ message: error.message });
+			}
+
+			return res
+				.setHeader('Content-Type', 'text/html')
+				.status(error.statusCode)
+				.send(`<p>${error.message}</p>`);
 		}
 	}
 
 	if (error instanceof HttpError) {
-		return res.status(error.statusCode).json({ message: error.message });
+		if (req.get('Content-Type') === 'application/json') {
+			return res.status(error.statusCode).json({ message: error.message });
+		}
+
+		return res
+			.setHeader('Content-Type', 'text/html')
+			.status(error.statusCode)
+			.send(`<p>${error.message}</p>`);
 	}
 
 	// Note: At this point, the error type is unknown, so we log it for further investigation.
 	logger.error(error);
 
-	return res.status(500).json({ message: 'Oh no, something went wrong!' });
+	const message = 'Oh no, something went wrong!';
+
+	if (req.get('Content-Type') === 'application/json') {
+		return res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message });
+	}
+
+	return res
+		.setHeader('Content-Type', 'text/html')
+		.status(statusCode.INTERNAL_SERVER_ERROR)
+		.send(`<p>${message}</p>`);
 }
 
 export function catchAsyncErrorMiddleware<P = any, ResBody = any, ReqBody = any, ReqQuery = any>(
@@ -91,9 +114,16 @@ export function rateLimitMiddleware(getIpAddress: (req: Request) => string) {
 		standardHeaders: 'draft-7',
 		legacyHeaders: false,
 		handler: (req, res) => {
+			const message = 'Too many requests, please try again later.';
+
+			if (req.get('Content-Type') === 'application/json') {
+				return res.status(statusCode.TOO_MANY_REQUESTS).json({ message });
+			}
+
 			return res
+				.setHeader('Content-Type', 'text/html')
 				.status(statusCode.TOO_MANY_REQUESTS)
-				.json({ message: 'Too many requests, please try again later.' });
+				.send(`<p>${message}</p>`);
 		},
 		skip: (req, res) => {
 			const ips = appConfig.IPS.split(', ');
