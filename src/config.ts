6c5ead8bfb1f444985ca6d8294605f84d7b8a7ec
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-04 18:09:58 -0500

    feat: discord envs

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,24 @@ import { validateConfig } from './util';
 
 dotenv.config({ path: path.resolve(path.join(process.cwd(), '.env')) });
 
+export const discordConfig = validateConfig({
+	DISCORD_ID: {
+		value: process.env.DISCORD_ID,
+		required: true,
+		type: (value: any) => String(value),
+	},
+	DISCORD_TOKEN: {
+		value: process.env.DISCORD_TOKEN,
+		required: true,
+		type: (value: any) => String(value),
+	},
+	DISCORD_URL: {
+		value: process.env.DISCORD_URL,
+		required: true,
+		type: (value: any) => String(value),
+	},
+});
+
 export const appConfig = validateConfig({
 	IPS: {
 		value: process.env.IPS,
