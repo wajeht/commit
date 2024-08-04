a90fce6f07b39efa31ee2b566086742787665d89
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-04 18:23:00 -0500

    feat: move types to its own folder

--- src/types.ts
+++ src/types.ts
@@ -32,3 +32,26 @@ export interface Logger {
 	error: (...value: any) => void;
 	info: (...value: any) => void;
 }
+
+export interface Embed {
+	title: string;
+	description: string;
+}
+
+export interface DiscordMessage {
+	username: string;
+	content: string;
+	embeds?: Embed[];
+}
+
+export interface Notifier {
+	discord(msg: string, object: any): Promise<void>;
+	email(to: string, subject: string, body: string): Promise<void>;
+}
+
+export interface NotifyParams {
+	discordUrl?: string;
+	environment?: string;
+	botUsername?: string;
+	httpClient?: (url: string, options: RequestInit) => Promise<Response>;
+}
