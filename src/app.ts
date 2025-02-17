4ddb794ae0a6b66dc2f3856a60aa55bf232e5bf3
wajeht <58354193+wajeht@users.noreply.github.com>
2025-02-16 22:23:35 -0600

    style: Refactor app initialization to method chaining style

--- src/app.ts
+++ src/app.ts
@@ -6,26 +6,16 @@ import compression from 'compression';
 import { getIpAddress } from './util';
 import { notFoundMiddleware, errorMiddleware, rateLimitMiddleware, helmet } from './middleware';
 
-const app = express();
-
-app.use(express.json({ limit: '1mb' }));
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(helmet());
-
-app.use(cors());
-
-app.use(compression());
-
-app.use(rateLimitMiddleware(getIpAddress));
-
-app.use(express.static(path.resolve(path.join(process.cwd(), 'public')), { maxAge: '30d' }));
-
-app.use(router);
-
-app.use(notFoundMiddleware);
-
-app.use(errorMiddleware());
+const app = express()
+	.use(express.json({ limit: '1mb' }))
+	.use(express.urlencoded({ extended: true }))
+	.use(helmet())
+	.use(cors())
+	.use(compression())
+	.use(rateLimitMiddleware(getIpAddress))
+	.use(express.static(path.resolve(path.join(process.cwd(), 'public')), { maxAge: '30d' }))
+	.use(router)
+	.use(notFoundMiddleware)
+	.use(errorMiddleware());
 
 export { app };
