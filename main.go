b8081f8bc9a12e312790fcc5c73910d931355fd9
wajeht <58354193+wajeht@users.noreply.github.com>
2025-10-28 16:35:02 -0500

    style(main): update cors middleware to allow only get and post methods

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func stripTrailingSlashMiddleware(next http.Handler) http.Handler {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == http.MethodOptions {
