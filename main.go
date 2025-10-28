02dc735c9d106afb6849c809794cf625f7594859
wajeht <58354193+wajeht@users.noreply.github.com>
2025-10-28 16:33:46 -0500

    feat(main): add cors middleware for handling cross-origin requests

--- main.go
+++ main.go
@@ -25,6 +25,21 @@ func stripTrailingSlashMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func handleHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
@@ -84,7 +99,7 @@ func main() {
 
 	server := &http.Server{
 		Addr:    serverAddr,
-		Handler: mux,
+		Handler: corsMiddleware(mux),
 	}
 
 	quit := make(chan os.Signal, 1)
