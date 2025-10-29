556c6d8dc947b2a391ee5ded29910c97aa0f0562
wajeht <58354193+wajeht@users.noreply.github.com>
2025-10-29 00:54:41 -0500

    refactor: remove unused commit.sh route

--- cmd/routes.go
+++ cmd/routes.go
@@ -13,7 +13,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /robots.txt", app.handleRobotsTxt)
 	mux.HandleFunc("GET /favicon.ico", app.handleFavicon)
 	mux.HandleFunc("GET /install.sh", app.handleInstallSh)
-	mux.HandleFunc("GET /commit.sh", app.handleHome)
 	mux.HandleFunc("GET /", app.handleHome)
 	mux.Handle("POST /", app.limitIPsMiddleware(http.HandlerFunc(app.handleGenerateCommit)))
 
